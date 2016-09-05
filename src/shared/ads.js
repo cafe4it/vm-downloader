905612e45d71436c42eba169f2d86e408fc3bdcc
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2016-09-05 11:40:24 +0700

    change ads + support brasil lang

--- src/shared/ads.js
+++ src/shared/ads.js
@@ -1,122 +1,32 @@
+import _ from 'lodash';
 const items = [
     {
-        id : 'http://s.click.aliexpress.com/e/ZN76Aa27E?bz=300*250',
-        src : 'http://img.alibaba.com/images/cms/upload/portals/300x2502.jpg',
+        id : 'http://s.click.aliexpress.com/e/23ZvJU7AE?bz=300*250',
+        src : 'http://ae01.alicdn.com/kf/HTB1e6WTHVXXXXcpXFXXq6xXFXXXQ/300x250.jpg',
         width : '300px',
-        height : '250px'
+        height : '250px',
+	    locale: 'EN'
     },
     {
-        id : 'http://s.click.aliexpress.com/e/qN7yJeiAU',
-        src : 'http://g01.a.alicdn.com/kf/HTB1MgAlJXXXXXb1aXXXq6xXFXXXZ/en-300x250.jpg',
-        width : '300px',
-        height : '250px'
+        id : 'http://s.click.aliexpress.com/e/2z7iMrrzr',
+        src : 'http://ae01.alicdn.com/kf/HTB14jyXHpXXXXchaXXXq6xXFXXXd/300x250.jpg',
+	    width : '300px',
+	    height : '250px',
+	    locale: 'RU'
     },
     {
-        id : 'http://s.click.aliexpress.com/e/2FuvN7eUZ?bz=500*500',
-        src : 'http://g01.a.alicdn.com/kf/HTB1ArwIJXXXXXXAXVXXq6xXFXXX5/电子商品-500x500.jpg',
-        width : '500px',
-        height : '500px'
+        id : 'http://s.click.aliexpress.com/e/y7yByRrZr?bz=300*250',
+        src : 'http://ae01.alicdn.com/kf/HTB1xHegHpXXXXXlXVXXq6xXFXXXA/300x250.jpg',
+	    width : '300px',
+	    height : '250px',
+	    locale: 'PT_BR'
     },
-    {
-        id : 'http://s.click.aliexpress.com/e/Mj2JIeqj2',
-        src : 'http://g01.a.alicdn.com/kf/HTB1SBn9JXXXXXX2XpXXq6xXFXXXl/婚礼-500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/IAUrnammA',
-        src : 'http://g01.a.alicdn.com/kf/HTB19YfTJXXXXXcAXVXXq6xXFXXXq/儿童-500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/jyn6mQZjU',
-        src : 'http://g01.a.alicdn.com/kf/HTB1qoDRJXXXXXX_aXXXq6xXFXXXM/运动-500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/uJuBAYNFI',
-        src : 'http://g01.a.alicdn.com/kf/HTB1VhkXJXXXXXXpXpXXq6xXFXXXx/家居-500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/QzjqfMZba?bz=500*500',
-        src : 'http://g01.a.alicdn.com/kf/HTB1ebfVJXXXXXbnXVXXq6xXFXXXi/男鞋-500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/FQ7Aqz3Zj?bz=500*500',
-        src : 'http://g01.a.alicdn.com/kf/HTB1G4P_JXXXXXX6XpXXq6xXFXXXG/玩具-500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/qJU7QZ7AY',
-        src : 'http://g01.a.alicdn.com/kf/HTB1822WJXXXXXbHXVXXq6xXFXXXI/女鞋-500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/jYZRzrjE6',
-        src : 'http://g01.a.alicdn.com/kf/HTB1rZPHJXXXXXcbXFXXq6xXFXXXI/首饰-500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/E6ImIiuZz',
-        src : 'http://g01.a.alicdn.com/kf/HTB1HqDQJXXXXXcVXXXXq6xXFXXXU/手机-500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/UfAEU3Nvj?bz=500*500',
-        src : 'http://g01.a.alicdn.com/kf/HTB1MhjTJXXXXXcfXXXXq6xXFXXX0/女包-500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/VRfeEqfAM',
-        src : 'http://g01.a.alicdn.com/kf/HTB1syDeJXXXXXXwXXXXq6xXFXXXA/女装-500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/6qRzVfiUj?bz=500*500',
-        src : 'http://g01.a.alicdn.com/kf/HTB1d2aqJXXXXXaOXXXXq6xXFXXXJ/男装-500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/7AUrZjMvz?bz=500*500',
-        src : 'http://g01.a.alicdn.com/kf/HTB1cAC5IVXXXXcCXFXXq6xXFXXXn/500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/yjaUFMFY3',
-        src : 'http://g01.a.alicdn.com/kf/HTB1An9OHVXXXXaCXVXXq6xXFXXXi/500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/QFeYNZfAA',
-        src : 'http://g01.a.alicdn.com/kf/HTB1fEz5GFXXXXbFaXXXq6xXFXXXK/500x500.jpg',
-        width : '500px',
-        height : '500px'
-    },
-    {
-        id : 'http://s.click.aliexpress.com/e/eeAuBmAUb',
-        src : 'http://gtms02.alicdn.com/tps/i2/T10Kh1FqJcXXbwqofS-300-250.jpg',
-        width : '300px',
-        height : '250px'
-    }
 ]
 
-module.exports = getRandom;
+module.exports = getByLocale;
 
-function getRandom(){
-    return items[Math.floor(Math.random()*items.length)];
+function getByLocale(){
+	var currentLocale = chrome.i18n.getMessage('lang');
+	var ads = _.find(items, function(i) { return i.locale.toUpperCase() === currentLocale.toLowerCase()});
+	return ads || items[0];
 }
